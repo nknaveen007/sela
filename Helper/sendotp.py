d6fd938a88a2842f17b733f6af74448ce5d6e5b7
nknaveen007 <connaveen007@gmail.com>
2021-09-14 10:37:54 -0700

    jason fix

--- Helper/sendotp.py
+++ Helper/sendotp.py
@@ -9,41 +9,33 @@ import asyncio
 
 account_sid = config('TWILIO_ACCOUNT_SID')
 auth_token = config('TWILIO_AUTH_TOKEN')
-twilio_service= config('SERVICES_TWILIO')
+twilio_service = config('SERVICES_TWILIO')
 client = Client(account_sid, auth_token)
 
+
 def sendOtpHelper(number):
-  print(number)
-  try:
-    verification = client.verify \
-                     .services(twilio_service) \
-                     .verifications \
-                  .create(to=number, channel='sms')
-    print(verification.status) 
-    return {'status':verification.status,'valid':verification.valid}   
-  except Exception as e:
-     print(e)
-     return {'status':'something wrong','valid':'null'}          
-  
-  
-  
+    print(number)
+    try:
+        verification = client.verify \
+            .services(twilio_service) \
+            .verifications \
+            .create(to=number, channel='sms')
+        print(verification.status)
+        return {'Status': True, 'Message': 'OTP sent to your given number '+number}
+    except Exception as e:
+        print(e)
+        return {'Status': False, 'Message': 'your number is not valid or something wrong'}
 
 
-def verifyOtpHelper(number,code):
-  print(number,code,'in verify')
-  try :
-    verification_check = client.verify \
-                           .services(twilio_service) \
-                           .verification_checks \
-                           .create(to=number, code=code)
-    print(verification_check.status,verification_check.valid)      
-    return {'status':verification_check.status,'valid':verification_check.valid}   
-  except:
-    return{'status':'something wrong','valid':'null'}                   
-    
- 
-  
-  
+def verifyOtpHelper(number, code):
+    print(number, code, 'in verify')
+    try:
+        verification_check = client.verify \
+            .services(twilio_service) \
+            .verification_checks \
+            .create(to=number, code=code)
+        print(verification_check.status, verification_check.valid)
 
-
-
+        return {'status': verification_check.status, 'valid': verification_check.valid}
+    except:
+        return{'Status': False, 'Message': 'Something wrong'}
